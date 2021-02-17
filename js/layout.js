9cadbbcd07b8950339fbaa1a5ae2b43f5b142c21
Irina Lagutina <ivl77@yandex.ru>
2021-02-17 22:08:53 +0300

    Добавила синхронизацию полей формы Тип жилья --> Цена и Время заезда и выезда

--- js/layout.js
+++ js/layout.js
@@ -39,44 +39,33 @@ const generateListOfImg = (array) => {
 };
 
 const map = document.querySelector('#map-canvas');
-const templateFragment = document.querySelector('#card').content;
-const template = templateFragment.querySelector('article');
+const template = document.querySelector('#card').content.querySelector('article');
 const fragment = document.createDocumentFragment();
 
 const createLayoutForRentObject = (object) => {
+  const { author: {avatar}, offer: {title, address, price, type, rooms, guests, checkin, features, description, photos} } = object;
   const element = template.cloneNode(true);
 
-  const title = element.querySelector('.popup__title');
-  object.offer.title ? title.textContent = object.offer.title : title.remove();
+  element.querySelector('.popup__title').textContent = title;
+  element.querySelector('.popup__text--address').textContent = address;
+  element.querySelector('.popup__text--price').innerHTML = `${price} <span>₽/ночь</span>`;
+  element.querySelector('.popup__type').textContent = convertRentObjectType(type);
+  element.querySelector('.popup__text--capacity').textContent = `${rooms} комнаты для ${guests} гостей`;
+  element.querySelector('.popup__text--time').textContent = `Заезд после ${checkin}, выезд до ${checkin}`;
 
-  const address = element.querySelector('.popup__text--address');
-  object.offer.address ? address.textContent = object.offer.address : address.remove();
+  const newFeatures = element.querySelector('.popup__features');
+  newFeatures.innerHTML = '';
+  features.length ? newFeatures.appendChild(generateListOfLi(features)) : newFeatures.remove();
 
-  const price = element.querySelector('.popup__text--price');
-  object.offer.price ? price.innerHTML = `${object.offer.price} <span>₽/ночь</span>` : price.remove();
+  const newDescription = element.querySelector('.popup__description');
+  description ? newDescription.textContent = description : newDescription.remove();
 
-  const type = element.querySelector('.popup__type');
-  object.offer.type ? type.textContent = convertRentObjectType(object.offer.type) : type.remove();
+  const newPhotos = element.querySelector('.popup__photos');
+  newPhotos.innerHTML = '';
+  photos.length ? newPhotos.appendChild(generateListOfImg(photos)) : newPhotos.remove();
 
-  const guests = element.querySelector('.popup__text--capacity');
-  object.offer.rooms ? guests.textContent = `${object.offer.rooms} комнаты для ${object.offer.guests} гостей` : guests.remove();
-
-  const time = element.querySelector('.popup__text--time');
-  object.offer.checkin ? time.textContent = `Заезд после ${object.offer.checkin}, выезд до ${object.offer.checkout}` : time.textContent = 'Заезд после 12:00, выезд до 12:00';
-
-  const features = element.querySelector('.popup__features');
-  features.innerHTML = '';
-  object.offer.features.length ? features.appendChild(generateListOfLi(object.offer.features)) : features.remove();
-
-  const description = element.querySelector('.popup__description');
-  object.offer.description ? description.textContent = object.offer.description : description.remove();
-
-  const photos = element.querySelector('.popup__photos');
-  photos.innerHTML = '';
-  object.offer.photos.length ? photos.appendChild(generateListOfImg(object.offer.photos)) : photos.remove();
-
-  const avatar = element.querySelector('.popup__avatar');
-  object.author.avatar ? avatar.src = object.author.avatar : avatar.remove();
+  const newAvatar = element.querySelector('.popup__avatar');
+  avatar ? newAvatar.src = avatar : newAvatar.remove();
 
   fragment.appendChild(element);
 
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import {createSimilarRentObjects as data} from './data.js';
 import {createLayoutForRentObject} from './layout.js';
+import './form.js';
 
 createLayoutForRentObject(data[0]);
